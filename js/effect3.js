688c165a13fe4a726adee03dc96674bd8d75a302
Teeb <teeb@MacBook-Pro-de-Teeb.local>
2017-05-13 00:23:21 +0100

    mandelbrot grey scale

--- js/effect3.js
+++ js/effect3.js
@@ -1,14 +1,17 @@
-function Graph(canvasId) {
-    var canvas = document.getElementById(canvasId);
-    var ctx = canvas.getContext("2d");
-    var imageData = ctx.createImageData(canvas.width, canvas.height);
-    var aspectRatio = canvas.height / canvas.width
-    this.r = 4
+
+function Mandelbrot(canvasId, x, y, r) {
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext("2d");
+    const imageData = ctx.createImageData(canvas.width, canvas.height);
+    const aspectRatio = canvas.height / canvas.width
+    // The more iterqtions we do the finer the grqin is in the rendering
+    this.iterations = 100;
+    this.r = r
     this.center = {
-        x: 0,
-        y: 0
+        x: x,
+        y: y
     };
-    var indexToCoord = function(index) {
+    const indexToCoord = function(index) {
         index /= 4;
         coord =  {
             x: index % canvas.width,
@@ -18,21 +21,41 @@ function Graph(canvasId) {
         coord.y = ((((coord.y * this.r / canvas.height) - this.r / 2) * -1) + this.center.y);
         return coord;
     }.bind(this)
-    
-    this.render = function(predicate) {
-        for (var i = 0; i < canvas.width * canvas.height * 4; i += 4) {
-            set = predicate(indexToCoord(i)) ? 255 : 0;
+    const isMandlebrot = function(coord) {
+        const cr = coord.x
+        const ci = coord.y
+        let zr = coord.x
+        let zi = coord.y
+        
+        for (let i = 0; i < this.iterations; i++) {
+            if (zr**2 + zi**2 > 4) {
+                return false;
+            }
+            newzr = (zr * zr) - (zi * zi) + cr;
+            newzi = ((zr * zi) *2) + ci
+            zr = newzr
+            zi = newzi
+        }
+        return true;
+    }.bind(this);
+    this.render = function() {
+        for (let i = 0; i < canvas.width * canvas.height * 4; i += 4) {
+            set = isMandlebrot(indexToCoord(i)) ? 255 : 0;
             imageData.data[i]     = 0;
             imageData.data[i + 1] = 0;
             imageData.data[i + 2] = 0;
             imageData.data[i + 3] = set;
         }
         ctx.putImageData(imageData, 0, 0);
-    }
+    }.bind(this)
 }
 
-var graph = new Graph("can_1");
-graph.r = 500;
-graph.render(function(coord) {
-    return (coord.x > 100 && coord.y > -27);
-});
+const graph = new Mandelbrot("can_1", -0.7463, 0.1102, 0.005);
+const graph2 = new Mandelbrot("can_2", -0.7453, 0.1127, 0.00065);
+const graph3 = new Mandelbrot("can_3", -1.25066, 0.02012, 0.0005);
+const graph4 = new Mandelbrot("can_4", -0.16, 1.0405, 0.046);
+
+graph.render();
+graph2.render();
+graph3.render();
+graph4.render(); 
--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -1,21 +1,18 @@
-function thinger(cr, ci) {
+function isMandelbrot(coord) {
+    const cr = coord.x;
+    const ci = coord.y;
     const zr = cr;
     const zi = ci;
-   
-    for(let i = 0; i < 100; i += 1) {
-        if(zr ** 2 + zi ** 2 > 4) {
+    for (var i = 0; i < 100; i++) {
+        if (zr**2 + zi**2 > 4) {
             return false;
         }
-
         newzr = (zr * zr) - (zi * zi) + cr;
         newzi = ((zr * zi) *2) + ci;
         zr = newzr;
-        zi = newzi; 
+        zi = newzi;
     }
-
     return true;
 }
-
-thinger(0, -1, 0);
-
-const
+var graph = new Graph("ex12");
+graph.render(isMandlebrot); 
